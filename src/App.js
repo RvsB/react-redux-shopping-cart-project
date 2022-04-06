00e61b9d81978b11b850efb9259944c6b20945b7
Raj Vardhan Singh Birani <rbrajvardhan@gmail.com>
2022-04-07 03:07:43 +0530

    feat: starter setup for the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		//BEM
+		<div className="app">
+			<h1>Helloo guys, this is a spotify clone</h1>
+			
+			
+			{/* Spotify Logo */}
+			{/* Login with spotify button */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import './Login.css';
+
+function Login() {
+	return (
+		<div>
+			{/* Spotify logo */}
+			{/* Login with spotify button */}
+		</div>
+	)
+}
+export default Login
